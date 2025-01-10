9b7140f93007eef37083425a1cc1b697b8e428fb
PondAnawin <anawin.pp@gmail.com>
2025-01-11 04:08:59 +0700

    delete folder mask_tool from branch

--- mask_tool/code/u2netfasttst.py
+++
@@ -1,6 +0,0 @@
-import torch
-from PIL import Image
-from u2net_fast.remover import Remover
-# Initialize the Remover with the path to the model weights
-remover = Remover(model_path='os.getcwd()}/models/u2net.pth')
-remover.batch_remove_background("mask_tool/test_source/cart.png","cart.png")
